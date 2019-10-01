d86af91652c00f974e43a424053978325d55d138
James Miller <james.rex.miller.4@gmail.com>
2019-10-01 00:53:04 -0600

    Begin the start method of Game.js and testing

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
-const data = require('./data');
+const data = require('../src/data');
 const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const util = require('../src/util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0
+    // this.data = prototypeQuestions
+  }
+  start(data) {
+    // const cards = []
+    // // const deck = new Deck ()
+    // console.log(data[i]);
+    // for (let i = 0; i < data.length; i++) {
+    //   var card = new Card(data[i])
+    //   cards.push(card)
+    // }
+    // const deck = new Deck(cards);
+    // console.log(deck[0])
+    // return deck;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
---
+++ test/Game-test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
+const util = require('../src/util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Game = require('../src/Game');
+
+
+describe('Game', function() {
+
+  it('should be a function', function() {
+    const game = new Game ();
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function() {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should keep track of the currentRound', function() {
+    const game = new Game();
+
+    game.start(prototypeQuestions);
+
+    expect(game.currentRound).to.equal(0);
+  });
+
+  it.skip('should create instances of Cards and put them into an instance of a Deck', function() {
+    const game = new Game();
+
+    // game.start(prototypeQuestions);
+    expect(game.start(prototypeQuestions)).to.equal(deck);
+      
+  })
+
+
+});
